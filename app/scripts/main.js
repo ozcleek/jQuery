98fd580e52b48cc2ff3109cb580d454ba5778392
Austin Cleek <austincleek@gmail.com>
2014-01-24 15:24:37 -0500

    jQuery project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,232 @@
 console.log('\'Allo \'Allo!');
+
+// Box 1
+
+$('.one').mouseover(function(){
+console.log('turngreen');
+
+$(this).toggleClass('toggle');
+});
+
+// Box 2
+
+$('.two').hover(function(){
+console.log('disappear');
+
+$(this).toggleClass('toggletwo');
+});
+
+// Box 3
+
+$('.three').dblclick(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 4
+
+$('.four').hover(function(){
+   
+   $(this).fadeTo(200, 3);
+});
+
+
+// Box 5
+
+$('.five').click(function(){
+   
+   $(this).fadeTo(5000, .5);
+});
+
+// Box 6
+
+$('.six').hover(function(){
+  
+   $(this).fadeOut('fast');
+   $(this).fadeIn(2000, 0);
+});
+
+
+// Box 7
+
+$('.seven').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 8
+
+$('.eight').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+
+// Box 9
+$('.nine').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 10
+
+$('.ten').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 11
+
+$('.eleven').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+
+// Box 12
+
+$('.twelve').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 13
+
+$('.thirteen').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 14
+
+$('.fourteen').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 15
+
+$('.fifteen').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 16
+
+
+$('.sixteen').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+
+// Box 17
+
+
+$('.seventeen').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 18
+
+
+$('.eighteen').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 19
+
+
+$('.nineteen').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 20
+
+$('.twenty').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 21
+
+$('.twentyone').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 22
+
+$('.twentytwo').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 23
+
+$('.twentythree').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+
+// Box 24
+
+$('.twentyfour').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+
+// Box 25
+
+$('.twentyfive').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 26
+
+$('.twentysix').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 27
+
+$('.twentyseven').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 28
+
+$('.twentyeight').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 29
+
+$('.twentynine').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+// Box 30
+
+$('.thirty').click(function(){
+   
+   $(this).fadeTo(1000, 0);
+});
+
+
+
+
+
+
+
+
